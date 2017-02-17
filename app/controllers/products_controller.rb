660da794c2d6733e210f022086236a664382e306
Askar Karasaev <askar.karasaev@gmail.com>
2017-02-17 11:59:48 +0900

    Add ability to view a list of all products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,6 @@
 class ProductsController < ApplicationController
   def index
-    #code
+    @products = Product.all
   end
 
   def new
---
+++ spec/features/viewing_products_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.feature 'Users can view products' do
+  scenario 'with the product details' do
+    product = FactoryGirl.create(:product, name: 'Shampoo', price: 11.22)
+    visit '/'
+    click_link 'Shampoo'
+    expect(page.current_url).to eq product_url(product)
+  end
+end
