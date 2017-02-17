0686d86f8a430f915e49661076c28c038e0a416b
Askar Karasaev <askar.karasaev@gmail.com>
2017-02-17 12:47:35 +0900

    Redirect users back to products page if they try going to a product that does not exist

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,6 @@
 class ProductsController < ApplicationController
+  before_action :set_product, only: [:show, :edit, :update, :destroy]
+
   def index
     @products = Product.all
   end
@@ -20,15 +22,12 @@ class ProductsController < ApplicationController
   end
 
   def show
-    @product = Product.find(params[:id])
   end
 
   def edit
-    @product = Product.find(params[:id])
   end
 
   def update
-    @product = Product.find(params[:id])
     if @product.update(product_params)
       flash[:notice] = 'Product has been updated.'
       redirect_to @product
@@ -39,7 +38,6 @@ class ProductsController < ApplicationController
   end
 
   def destroy
-    @product = Product.find(params[:id])
     @product.destroy
 
     flash[:notice] = 'Product has been deleted.'
@@ -47,6 +45,12 @@ class ProductsController < ApplicationController
   end
 
   private
+  def set_product
+    @product = Product.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    flash[:alert] = "The product you were looking for could not be found."
+    redirect_to products_path
+  end
   def product_params
     params.require(:product).permit(:name, :description, :price)
   end
--- spec/controllers/products_controller_spec.rb
+++ spec/controllers/products_controller_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe ProductsController, type: :controller do
+  it 'handles a missing product correctly' do
+    get :show, id: 'not-here'
 
+    expect(response).to redirect_to(products_path)
+
+    message = 'The product you were looking for could not be found.'
+    expect(flash[:alert]).to eq message
+  end
 end
