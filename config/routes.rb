eac31e2573a2ae872dfcc605451f0dae6debbbc2
Askarbek Karasasev <askar.karasaev@gmail.com>
2017-02-18 22:57:40 +0900

    Add admin namespace with application controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    root "application#index"
+  end
+
   devise_for :users
 
   root :to => 'products#index'
-  
+
   resources :products
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -54,6 +54,7 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+  config.include Devise::Test::ControllerHelpers, type: :controller
   config.include Warden::Test::Helpers, type: :feature
   config.after(type: :feature) { Warden.test_reset! }
 end
