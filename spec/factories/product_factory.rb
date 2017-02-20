57924c0b7bf6c2cfd8a929672d6454443fa981b8
Askar Karasaev <askar.karasaev@gmail.com>
2017-02-20 11:11:28 +0900

    Only admins can see the links to create and delete products

--- spec/factories/product_factory.rb
+++ spec/factories/product_factory.rb
@@ -1,5 +1,6 @@
 FactoryGirl.define do
   factory :product do
     name 'Example product'
+    price 33.44
   end
 end
---
+++ spec/features/hidden_links_spec.rb
@@ -0,0 +1,43 @@
+
+feature 'Users can only see the appropriate links' do
+  let(:product) { FactoryGirl.create(:product) }
+  let(:user) { FactoryGirl.create(:user) }
+  let(:admin) { FactoryGirl.create(:user, :admin) }
+
+  context "anonymous users" do
+    scenario "cannot see the New Product link" do
+      visit '/'
+      expect(page).not_to have_link 'New Product'
+    end
+    scenario "cannot see the Delete Product link" do
+      visit product_path(product)
+      expect(page).not_to have_link "Delete Product"
+    end
+  end
+
+  context "reguler users" do
+    before { login_as(user) }
+
+    scenario "cannot see the New Product link" do
+      visit "/"
+      expect(page).not_to have_link "New Product"
+    end
+    scenario "cannot see the Delete Product link" do
+      visit product_path(product)
+      expect(page).not_to have_link "Delete Product"
+    end
+  end
+
+  context "admin users" do
+    before { login_as(admin) }
+
+    scenario "can see the New Product link" do
+      visit '/'
+      expect(page).to have_link 'New Product'
+    end
+    scenario "cannot see the Delete Product link" do
+      visit product_path(product)
+      expect(page).to have_link "Delete Product"
+    end
+  end
+end
